4ab0cdbf91e5fdd4734672d48c9c7e3ae4be0d61
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-09-05 15:27:13 +0330

    add js

--- JS/mine.js
+++ JS/mine.js
@@ -1,1 +1,12 @@
-console.log('hanieh')
+
+const container = document.querySelector('.container')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+
+registerLink.addEventListener('click' , ()=> {
+    container.classList.add('active')
+})
+
+loginLink.addEventListener('click' , ()=> {
+    container.classList.remove('active')
+})
